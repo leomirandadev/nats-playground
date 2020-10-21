1ce84483edaeaa6bc8a5f06c17f3d1d73b4349f6
leomiranda-tc <leonardo.miranda@tradersclub.com.br>
2020-10-21 12:20:32 -0300

    go client

---
+++ go-client/go.mod
@@ -0,0 +1,5 @@
+module github.com/leomirandadev/nats-playground/go-client
+
+go 1.14
+
+require github.com/nats-io/nats.go v1.10.0 // indirect
---
+++ go-client/main.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"time"
+
+	nats "github.com/nats-io/nats.go"
+)
+
+type msg struct {
+	Client  string
+	Message string
+}
+
+func main() {
+
+	var channel string = "cannel-test"
+	var clientName string = generateClientName()
+
+	c := connectNats()
+
+	sendMessage(c, channel, msg{Client: clientName, Message: "The Dad's on"})
+
+	subscribeChannel(c, channel)
+
+	continueListen()
+
+}
+
+func generateClientName() string {
+	clientName := "client-" + strconv.FormatInt(time.Now().UnixNano(), 10)
+	fmt.Println("Client name:", clientName)
+
+	return clientName
+}
+
+func continueListen() {
+	fmt.Println("Listen")
+	select {}
+}
+
+func connectNats() *nats.EncodedConn {
+	nc, err := nats.Connect(nats.DefaultURL)
+
+	if err != nil {
+		fmt.Println("Error on conection")
+	}
+
+	fmt.Println("Connected on", nats.DefaultURL)
+
+	c, _ := nats.NewEncodedConn(nc, nats.JSON_ENCODER)
+
+	return c
+}
+
+func sendMessage(c *nats.EncodedConn, channel string, message msg) {
+
+	err1 := c.Publish(channel, message)
+
+	if err1 != nil {
+		fmt.Println("Error on publish message")
+	}
+
+	fmt.Println("Send message to:", channel)
+}
+
+func subscribeChannel(c *nats.EncodedConn, channel string) {
+
+	_, err2 := c.Subscribe(channel, func(m *nats.Msg) {
+		fmt.Printf("Received a message: %s\n", string(m.Data))
+	})
+
+	if err2 != nil {
+		fmt.Println("Error on subcribe on a chanel")
+	}
+
+	fmt.Println("Subscribe successfuly on", channel)
+}
